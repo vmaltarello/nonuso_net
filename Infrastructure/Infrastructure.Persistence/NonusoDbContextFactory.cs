7df6e3fb39d5c04781eb80bf9aab4fad23ee8742
Vladimir Alberto Maltarello <vmaltarello@gmail.com>
2025-05-30 13:06:35 +0200

    -

--- Infrastructure/Infrastructure.Persistence/NonusoDbContextFactory.cs
+++ Infrastructure/Infrastructure.Persistence/NonusoDbContextFactory.cs
@@ -1,12 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace Nonuso.Infrastructure.Persistence
 {
